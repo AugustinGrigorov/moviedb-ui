25d39314980777003ebf5158b662f01c740d6249
Augustin Grigorov <augustingrigorov@Augustins-MacBook-Air.local>
2017-07-01 19:40:42 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="MovieDB UI">
+        <h2>Welcome to the Movie Database UI</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
